ced7390ba3069e735ad561b0bb844bf10e03141e
Galiwango Ananiya <galiwango101@gmail.com>
2022-12-03 01:27:59 +0300

    add basic search funtionality to the controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
   def index
+    if params[:query].present?
+      @articles = Article.where("title LIKE ?", "#{params[:query]}%")
+    else
+      @articles = Article.all
+    end
   end
 end
