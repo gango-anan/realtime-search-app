3dc51d0a78b3f57bcee5cbed3dc2759d2b444551
Galiwango Ananiya <galiwango101@gmail.com>
2022-12-03 01:26:24 +0300

    seed the database with some articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+[
+    {title: "Learning Ruby the hard way", body: "The most read ruby article"},
+    {title: "Learning Java the hard way", body: "The most read Java article"},
+    {title: "Learning Rails the hard way", body: "The most read Rails article"},
+    {title: "Learning JavaScript the hard way", body: "The most read JavaScript article"},
+    {title: "Learning SQL the hard way", body: "The most read SQL article"},
+    {title: "Learning Python the hard way", body: "The most read Python article"},
+    {title: "Learning Golang the hard way", body: "The most read Golang article"},
+    {title: "Learning React the hard way", body: "The most read React article"},
+    {title: "Learning TypeScript the hard way", body: "The most read TypeScript article"},
+    {title: "Learning C++ the hard way", body: "The most read C++ article"}
+].each { |article| Article.create(article) }
+
