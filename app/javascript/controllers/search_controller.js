d1442ea517d9dadf4c7938bdcdf9ace290edc15e
Galiwango Ananiya <galiwango101@gmail.com>
2022-12-03 18:15:44 +0300

    complete implementing the real time search feature

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,14 +1,11 @@
 import { Controller } from "@hotwired/stimulus";
-import { debounce } from "debounce";
-
 
 // Connects to data-controller="search"
 export default class extends Controller {
-  initialize() {
-    this.submit = debounce(this.submit.bind(this), 300);
-  }
-
-  submit(event) {
-    this.element.requestSubmit();
+  submit() {
+    clearTimeout(this.timeout)
+    this.timeout = setTimeout(() => {
+      this.element.requestSubmit();
+    }, 500)
   }
 }
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
+    attr_accessor :exact_match
     def self.search_articles(params)
-        params[:query].blank? ? all : where("title LIKE ?", "%#{sanitize_sql_like(params[:query])}%")
+        params[:query].blank? ? [] : where("title LIKE ?", "%#{sanitize_sql_like(params[:query])}%")
     end
 end
