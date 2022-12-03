35ccd0b79b00899bec65f546daf3ec83be57028d
Galiwango Ananiya <galiwango101@gmail.com>
2022-12-03 20:20:38 +0300

    implement feature to add a SearchHistory if its valid

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.search_articles(params)
+    @search_history_results = SearchHistory.search_history(params)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,13 @@
 class Article < ApplicationRecord
     def self.search_articles(params)
-        params[:query].blank? ? [] : where("title LIKE ?", "%#{sanitize_sql_like(params[:query])}%")
+        @search_results = params[:query].blank? ? [] : where("title LIKE ?", "%#{sanitize_sql_like(params[:query])}%")
+    end
+
+    def self.search_history(params)
+        if @search_results && @search_results.length == 1 && @search_results[0].title == params[:query]
+            SearchHistory.create(article_id: @search_results[0].id)
+        end
+
+        SearchHistory.all
     end
 end
