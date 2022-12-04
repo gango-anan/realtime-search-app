35981abcc32c25922e8bfc425fef59053521f464
Galiwango Ananiya <galiwango101@gmail.com>
2022-12-04 23:05:53 +0300

    add test for analyzed_search_history method of the SearchHistory model

--- spec/models/search_history_spec.rb
+++ spec/models/search_history_spec.rb
@@ -8,4 +8,29 @@ RSpec.describe SearchHistory, type: :model do
   describe 'association' do
     it { should belong_to(:article) }
   end
+
+  describe 'analyzed_search_history' do
+    before(:all) do
+      [
+        {title: "Learning Ruby", body: "The most read ruby article"},
+        {title: "Learning Java", body: "The most read Java article"},
+      ].each { |article| Article.create(article) }
+      [
+        {article_id: 1},
+        {article_id: 2},
+        {article_id: 2},
+        {article_id: 2},
+      ].each { |search| SearchHistory.create(search) }
+
+      @results = SearchHistory.new.analyzed_search_history
+    end
+
+    it 'querry executes successfully' do
+      expect(@results).not_to be_empty
+    end
+
+    it 'it should return a list of articles with the number of searches' do
+      expect(@results[0]["number_of_times_searched"]).to be(3)
+    end
+  end
 end
