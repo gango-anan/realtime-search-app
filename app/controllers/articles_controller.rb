4c47d5228c748d8ffbfd7f424ee43ae6264e63fa
Galiwango Ananiya <galiwango101@gmail.com>
2022-12-04 16:17:30 +0300

    implement a better way to analyse and display searched articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.search_articles(params)
-    @search_histories = Article.search_history(params)
+    @search_histories = SearchHistory.new.analyzed_search_history.to_a
   end
 end
--- app/models/search_history.rb
+++ app/models/search_history.rb
@@ -1,3 +1,19 @@
 class SearchHistory < ApplicationRecord
     belongs_to :article
+
+    def analyzed_search_history
+        ActiveRecord::Base.connection.exec_query(query_searched_articles)
+    end
+
+    private
+
+    def query_searched_articles
+        <<-SQL
+            SELECT articles.title, COUNT(*) AS number_of_times_searched
+            FROM articles
+            JOIN search_histories ON search_histories.article_id = articles.id
+            GROUP BY articles.title
+            ORDER BY number_of_times_searched DESC;
+        SQL
+    end
 end
