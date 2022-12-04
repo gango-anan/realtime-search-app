9e153d64cae49a68cf38e28f52324d99ee82f1b9
Galiwango Ananiya <galiwango101@gmail.com>
2022-12-04 16:23:04 +0300

    refactor application_helper to remove unused code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-    def search_count(results)
-        search_history_counter = Hash.new(0)
-        results.each do |result|
-            search_history_counter[result.article.title] += 1
-        end
-
-        search_history_counter
-    end
 end
