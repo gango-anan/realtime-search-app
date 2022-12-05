21e4145a65e68095511325428b72b1b5a13579b3
Galiwango Ananiya <galiwango101@gmail.com>
2022-12-05 09:09:54 +0300

    add forms to populate the app with articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,22 @@ class ArticlesController < ApplicationController
     @articles = Article.search_articles(params)
     @search_histories = SearchHistory.new.analyzed_search_history.to_a
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(articles_params)
+    if @article.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def articles_params
+    params.require(:article).permit(:title, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles, only: %i[index]
-  root "articles#index"
+  resources :articles, only: %i[index, new, create]
+  resources :search_histories, only: %i[new create]
+  root "articles#new"
 end
