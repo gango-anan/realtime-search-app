8bde6abd683054c9fbcffcc5f0211f58f7fe4ef3
Galiwango Ananiya <galiwango101@gmail.com>
2022-12-05 07:13:04 +0300

    fix errors raised during deployment

--- app/models/search_history.rb
+++ app/models/search_history.rb
@@ -3,6 +3,18 @@ class SearchHistory < ApplicationRecord
     validates :article_id, presence: true
 
     def analyzed_search_history
-        []
+        ActiveRecord::Base.connection.exec_query(query_searched_articles)
+    end
+
+    private
+
+    def query_searched_articles
+        <<-SQL
+            SELECT articles.title, COUNT(*) AS number_of_times_searched
+            FROM articles
+            JOIN search_histories ON search_histories.article_id = articles.id
+            GROUP BY articles.title
+            ORDER BY number_of_times_searched DESC;
+        SQL
     end
 end
